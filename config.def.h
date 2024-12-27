6362cfe9ee412c4f71ec7892341b3c73f4f7a995
guebe <57858743+guebe@users.noreply.github.com>
2024-12-27 21:05:48 +0100

    changed font to Terminus

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Terminus:pixelsize=24:lcdfilter=lcddefault:hintstyle=hintnone:rgba=rgb:antialias=false:autohint=false";
 static int borderpx = 2;
 
 /*
